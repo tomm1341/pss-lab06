3d39dfe4328dcb9ce7036e93be9d9bf3c31c73f4
tomm1341 <tommaso.foca@studio.unibo.it>
2023-10-31 10:32:55 +0100

    Created ExtendedStrictBankAccount and initialized the class

---
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -0,0 +1,14 @@
+package it.unibo.inheritance.impl;
+
+public class ExtendedStrictBankAccount extends SimpleBankAccount{
+
+    private int transactions = 0;
+
+    public ExtendedStrictBankAccount(int id, double balance) {
+        super(id, balance);
+        //TODO Auto-generated constructor stub
+    }
+
+
+    
+}
