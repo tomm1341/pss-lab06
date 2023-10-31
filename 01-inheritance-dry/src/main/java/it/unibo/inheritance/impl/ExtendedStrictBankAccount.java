b6f5de6dc891e132a8038969e8758dce7d7ad3f2
tomm1341 <tommaso.foca@studio.unibo.it>
2023-10-31 11:28:21 +0100

    Created isWithdrawAllowed, transactionOp and withdraw function

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -2,13 +2,33 @@ package it.unibo.inheritance.impl;
 
 public class ExtendedStrictBankAccount extends SimpleBankAccount{
 
-    private int transactions = 0;
+    private static final double ATM_TRANSACTION_FEE = 1;
+    private static final double MANAGEMENT_FEE = 5;
+    private static final double TRANSACTION_FEE = 0.1;
+ 
+
 
     public ExtendedStrictBankAccount(int id, double balance) {
         super(id, balance);
-        //TODO Auto-generated constructor stub
+
+    }
+    
+    @Override
+    public void withdraw(int id, double amount) {
+        if(isWithdrawAllowed(amount)) {
+            withdraw(id, -amount);
+        }
     }
 
+    private void transactionOp(int id, double amount){
+        if(checkUser(id)) {
+            this.balance += amount;
+            this.incrementTransactions();
+        }
+    }
 
+    private boolean isWithdrawAllowed(final double amount) {
+        return this.balance >= amount;
+    }
     
 }
