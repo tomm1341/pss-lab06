20849549dfd3bf6fe4270d69d36e1f61174d6d16
tomm1341 <tommaso.foca@studio.unibo.it>
2023-11-06 10:09:12 +0100

    Deleted transactionOp because useless

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -2,7 +2,6 @@ package it.unibo.inheritance.impl;
 
 public class ExtendedStrictBankAccount extends SimpleBankAccount{
 
-    private static final double ATM_TRANSACTION_FEE = 1;
     private static final double MANAGEMENT_FEE = 5;
     private static final double TRANSACTION_FEE = 0.1;
  
@@ -33,14 +32,7 @@ public class ExtendedStrictBankAccount extends SimpleBankAccount{
         final double feeAmount = MANAGEMENT_FEE + transactions * ExtendedStrictBankAccount.TRANSACTION_FEE;
         if(checkUser(id) && isWithdrawAllowed(feeAmount)) {
             balance -= feeAmount;
-            transactions = 0;
-        }
-    }
-
-    private void transactionOp(final int id, double amount) {
-        if(checkUser(id)) {
-            this.balance += amount;
-            super.incrementTransactions();
+            super.resetTransactions();
         }
     }
 
